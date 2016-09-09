0a52b61d74dc2b54985a77d93fd326c6e644cb2e
Klunder <klunder@7831c1cdf79a.ant.amazon.com>
2016-09-09 10:12:26 -0700

    add testing code for full screen

--- app.js
+++ app.js
@@ -77,3 +77,31 @@ function updateDashboardDisplay(code, email) {
 function updateLocalStorage() {
 
 }
+
+
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+var fsBtn = document.getElementById('fs');
+fsBtn.addEventListener('click', function () {
+  launchIntoFullscreen(document.getElementById('container'));
+})
